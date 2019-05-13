be3e7dcfbffb0475d5c3be6ef8aab16246eaffe1
brakebu <brakebu@users.noreply.github.com>
2019-05-13 12:39:27 -0400

    add tileLayer

--- js/leafletdemo.js
+++ js/leafletdemo.js
@@ -4,3 +4,7 @@ var map = L.map('map', {
   center: [51.505, -0.09],
   zoom: 13
 });
+
+//enter tile layer
+
+var maptile = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'}).addTo(map);
