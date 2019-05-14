91f51410ceab32e39caf251a4592d918d946fc18
brakebu <brakebu@users.noreply.github.com>
2019-05-13 21:52:38 -0400

    Uploading geojson

--- js/leafletdemo.js
+++ js/leafletdemo.js
@@ -1,10 +1,23 @@
 //leaflet map js
 
 var map = L.map('map', {
-  center: [51.505, -0.09],
-  zoom: 13
+  center: [33.55, -82.216],
+  zoom: 17,
+  zoomControl: false,
+  minZoom: 13,
+  maxZoom: 20
 });
 
 //enter tile layer
 
 var maptile = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'}).addTo(map);
+
+//load GeoJSON file 
+
+function addDataToMap(data, map) {
+  var datalayer = L.geoJson(data);
+  datalayer.addTo(map);
+}
+
+$.getJSON("churchfamilymembers.geojson", function(data) { addDataToMap(data, map); });
+
